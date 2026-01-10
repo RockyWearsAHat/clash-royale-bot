9346344bd7bb12cfe11fc6d37e1d6ae9a37b5697
Rocky <alexwaldmann2004@gmail.com>
2026-01-10 16:27:33 -0700

    default commit message

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,6 @@ import { maybeRunNicknameToTagMigration } from './discord/nicknameMigration.js';
 import { listGuildMembersPage } from './discord/guildMembers.js';
 import { dbDeleteJobState, dbGetJobState, dbSetJobState } from './db.js';
 
-console.log('Starting bot...');
-
 const cfg = loadConfig();
 const db = openDb(cfg.SQLITE_PATH);
 migrate(db);
